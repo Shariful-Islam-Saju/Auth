b157b510459ae841b8c3ade575f3b15210bce0e2
Shariful Islam <sajukhan12905@gmail.com>
2025-02-03 19:57:02 +0600

    create dashboard and navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased  bg-gray-100`}
       >
-        <MaxWidthWrapper>{children}</MaxWidthWrapper>
+        <Navbar />
+        <MaxWidthWrapper className="mt-20">{children}</MaxWidthWrapper>
       </body>
     </html>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+// components/Navbar.tsx
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-gray-300 p-4 fixed top-0 w-full z-10">
+      <MaxWidthWrapper>
+        <div className="max-w-7xl mx-auto  flex justify-between items-center">
+          <Link href="/" className=" text-2xl font-semibold">
+            MyLogo
+          </Link>
+          <div className="space-x-4">
+            <Link href="/" className="">
+              Home
+            </Link>
+            <Link href="/dashboard" className="">
+              Dashboard
+            </Link>
+            <Link href="/register" className="">
+              Register
+            </Link>
+            <Link href="/login" className="">
+              Login
+            </Link>
+           
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
